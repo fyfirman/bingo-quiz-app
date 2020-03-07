c1f4617937257a9b9735ac140770bc8334e282cc
Firmansyah Yanuar <fyfirman@gmail.com>
2020-03-07 12:48:32 +0700

    create board

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import "antd/dist/antd.css";
+import Field from './component/Field';
+import { Button, Row, Col } from 'antd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    const boardSize = {
+      row: 5,
+      col: 5
+    };
+    const board = [];
+    for (var i = 0; i < boardSize.row; i++) {
+      var rows = [];
+      for (var j = 0; j < boardSize.col; j++)
+        rows.push(
+          <Col key={1} span={4}>
+            <Field />
+          </Col>
+        );
+      board.push(<Row gutter={[8, 8]}> {rows} </Row>);
+    }
+    return (
+      <div>
+        {board}
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/component/Field.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Field extends React.Component {
+  render() {
+    return (
+      <a href="#">
+        <div className="field" style={{backgroundColor: "lightblue"}}>
+          tes
+        </div>
+      </a >
+    );
+  }
+}
