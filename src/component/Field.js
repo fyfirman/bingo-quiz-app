ec5ef1460e25ccaf668f3c37511e46f181c99fb8
Firmansyah Yanuar <fyfirman@gmail.com>
2020-03-07 23:12:07 +0700

    update color when clicked

--- src/component/Field.js
+++ src/component/Field.js
@@ -2,12 +2,19 @@ import React from 'react';
 import { Modal } from 'antd';
 
 export default class Field extends React.Component {
-  state = { visible: false };
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false,
+      backgroundColor: 'lightblue'
+    };
+  }
 
   handleOk = e => {
     console.log(e);
     this.setState({
       visible: false,
+      backgroundColor: 'red'
     });
   };
 
@@ -24,23 +31,23 @@ export default class Field extends React.Component {
     });
   };
 
-  render() {
-    return (
-      <div>
-        <Modal
-          title="Topic: Geology"
-          visible={this.state.visible}
-          onOk={this.handleOk}
-          onCancel={this.handleCancel}
-        >
-          <p>When a source rock is examined under a reflectance microscope, the amount of light reflected by this plant organic matter helps define the maturity of the source rock. Name this organic matter! </p>
-        </Modal>
-        <a onClick={this.showModal}>
-          <div className="field" style={{ backgroundColor: "lightblue" }}>
-            tes
+    render() {
+  return (
+    <div>
+      <Modal
+        title="Topic: Geology"
+        visible={this.state.visible}
+        onOk={this.handleOk}
+        onCancel={this.handleCancel}
+      >
+        <p>When a source rock is examined under a reflectance microscope, the amount of light reflected by this plant organic matter helps define the maturity of the source rock. Name this organic matter! </p>
+      </Modal>
+      <a onClick={this.showModal}>
+        <div className="field" style={{ backgroundColor: this.state.backgroundColor }}>
+          tes
           </div>
-        </a >
-      </div>
-    );
-  }
+      </a >
+    </div>
+  );
+}
 }
