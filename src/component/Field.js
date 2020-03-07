89fbe0580301ecb2743de4a2021af4ecdb393987
Firmansyah Yanuar <fyfirman@gmail.com>
2020-03-07 22:50:17 +0700

    Add handle ok and cancel

--- src/component/Field.js
+++ src/component/Field.js
@@ -1,13 +1,46 @@
 import React from 'react';
+import { Modal } from 'antd';
 
 export default class Field extends React.Component {
+  state = { visible: false };
+
+  handleOk = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+  handleCancel = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
   render() {
     return (
-      <a href="#">
-        <div className="field" style={{backgroundColor: "lightblue"}}>
-          tes
-        </div>
-      </a >
+      <div>
+        <Modal
+          title="Topic: Geology"
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+        >
+          <p>When a source rock is examined under a reflectance microscope, the amount of light reflected by this plant organic matter helps define the maturity of the source rock. Name this organic matter! </p>
+        </Modal>
+        <a onClick={this.showModal}>
+          <div className="field" style={{ backgroundColor: "lightblue" }}>
+            tes
+          </div>
+        </a >
+      </div>
     );
   }
 }
